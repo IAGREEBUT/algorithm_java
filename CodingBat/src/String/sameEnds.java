9694e728dcf055b055b355371deff887db150a73
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-14 20:56:20 +0900

    String - 3 : sameEnds

--- CodingBat/src/String/sameEnds.java
+++ CodingBat/src/String/sameEnds.java
@@ -3,57 +3,54 @@ package String;
 public class sameEnds {
 
 
-    public static String sameEnds(String string) {
+    public static String sameEnds1(String string) {
 
         String ans="";
         int size = string.length();
-
-        if(size%2 ==0 ){ //길이가 짝수
-            int left = 0, right = size/2;
-            while(left<size/2 && right<size){
-
-                if(string.charAt(left)!=string.charAt(right)){
-                    right++;
-                }
-                else {
-                    ans+=string.charAt(left);
-                    left++;
-                    right++;
-                }
-
-            }
-//            System.out.println("짝수 인 경우\n");
-//            System.out.println(cnt);
-        }
-        else{
-            int left = 0, right = size/2 +1;
-            while(left<size/2 && right<size){
-
-                if(string.charAt(left)!=string.charAt(right)){
-                    right++;
-                }
-                else {
-                    ans+=string.charAt(left);
-                    left++;
-                    right++;
-                }
-
-            }
-//            System.out.println("홀 수 인 경우\n");
-//            System.out.println(cnt);
+        int left = 0, right = size/2;
+        if(size%2==1){
+            right++;
         }
 
+        //투포인터 탐색
+        while(left<size/2 && right<size){
 
-//        for(int i=0; i<cnt; i++){
-//            ans+=string.charAt(i);
-//        }
+            if(string.charAt(left)!=string.charAt(right)){
+                right++;
+            }
+            else {
+                ans+=string.charAt(left);
+                left++;
+                right++;
+            }
+
+        }
 
         return ans;
     }
 
+
+
+    public static String sameEnds2(String string){
+
+        int len = string.length();
+        String ans = "";
+        String tmp = "";
+
+        for(int i=0; i<len; i++){
+            tmp += string.charAt(i);
+            int tmplen = tmp.length();
+            if( i<len/2 && tmp.equals(string.substring(len-tmplen,len)))
+                ans = tmp;
+        }
+        return ans;
+
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(sameEnds("xxx"));
+        System.out.println(sameEnds1("abxyab"));
+        System.out.println(sameEnds2("xxx"));
 
 
     }
