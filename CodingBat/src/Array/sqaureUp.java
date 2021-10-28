ae4d1ff9fb2d9de736386497e693cc151ff9adbe
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-28 14:49:43 +0900

    Array - 3 : squareUp
    3번째 방법

--- CodingBat/src/Array/sqaureUp.java
+++ CodingBat/src/Array/sqaureUp.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class sqaureUp {
 
     /*
+    답은 1차원 array이지만, 생각(이해)은 2차원 배열으로 하는게 편함
+
     i행에서 가장 큰 수는 "i", 가장 작은 수는 1
 
      */
@@ -33,6 +35,22 @@ public class sqaureUp {
             }
         }
         return ans;
+    }
+
+
+    // https://github.com/mirandaio/codingbat/blob/master/java/array-3/squareUp.java
+    public static int[] squareUp3(int n) {
+        int[] arr = new int[n*n];
+
+        if(n == 0)
+            return arr;
+
+        for(int i = n - 1; i < arr.length; i += n) {//i : 행의 마지막 열의 index (0부터 시작이므로 n-1)
+            for(int j = i; j >= i - i / n; j--) //가야하열는 만큼 앞으로 가기위해 -- : 1행부터 시작이라고 가정, i/n = 1행에서 0, 2행에서 1 ,..
+                arr[j] = i - j + 1;//행의 마지막 열 - 행에서 수를 삽입할 차례인 열 + 1
+        }
+
+        return arr;
     }
 
     public static void main(String[] args) {
