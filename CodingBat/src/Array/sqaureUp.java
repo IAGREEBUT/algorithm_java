b23d77c29f70d284b214a2e08a92fd9c3ae7a03b
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-28 14:32:07 +0900

    Array - 3 : squareUp
    2번째 방법

--- CodingBat/src/Array/sqaureUp.java
+++ CodingBat/src/Array/sqaureUp.java
@@ -22,12 +22,25 @@ public class sqaureUp {
         return ans;
     }
 
+    public static int[] squareUp2(int n){
+        int[] ans = new int[n * n]; // n*n으로 배열 초기화(0으로 초기화됨) & 선언
+
+        int idx=0;
+        for(int i=1;i<=n;i++){ //1행~n행 까지
+            for(int j=n; j>=1; j--){ //i행은 n열을 보유함
+                if(j==i) idx = i*(n-1); //최초로 0이 아닌 수는 i * (n-1) 인덱스의 위치임
+                if(j<=i) ans[idx++] = j; // 인덱스가 정해진 이후로는 j를 대입해줌
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
 
 
-        System.out.println(Arrays.toString(squareUp(2)));
-        System.out.println(Arrays.toString(squareUp(3)));
-        System.out.println(Arrays.toString(squareUp(4)));
+//        System.out.println(Arrays.toString(squareUp2(2)));
+//        System.out.println(Arrays.toString(squareUp2(3)));
+        System.out.println(Arrays.toString(squareUp2(4)));
 
 
 
