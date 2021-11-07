f2e472b1c2dc5205b0253a51a80551b7cc233ded
YujeongLEE <dldbwjd24@ewhain.net>
2021-11-07 23:30:44 +0900

    Warmup2 : array123

--- CodingBat/src/Warmup/array123.java
+++ CodingBat/src/Warmup/array123.java
@@ -16,7 +16,7 @@ public class array123 {
 
     }
 
-    public static void main(){
+    public static void main(String[] args){
 
         int[] nums1 = {1,2,3,4,5};
         int[] nums2 = {1, 1, 2, 4, 1};
