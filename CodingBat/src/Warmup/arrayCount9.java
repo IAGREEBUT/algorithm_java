71661e1434bb5cdeed647b6d57b6c983ac51b12a
YujeongLEE <dldbwjd24@ewhain.net>
2021-11-20 15:48:32 +0900

    Warmup 2 : arrayCount9

--- CodingBat/src/Warmup/arrayCount9.java
+++ CodingBat/src/Warmup/arrayCount9.java
@@ -1,4 +1,26 @@
 package Warmup;
 
 public class arrayCount9 {
+
+
+    public static int arrayCount9(int[] nums){
+
+        int ans=0;
+
+        for(int i=0; i<nums.length;i++){
+            if(nums[i]==9) ans++;
+        }
+
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {9,2,3};
+
+        System.out.println(arrayCount9(nums));
+
+    }
+
 }
