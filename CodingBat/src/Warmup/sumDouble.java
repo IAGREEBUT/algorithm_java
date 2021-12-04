0449d905f6a75ed2c62ae8a5db4daec913e27d7f
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-04 23:47:53 +0900

    Warmup 1 : sumDouble

---
+++ CodingBat/src/Warmup/sumDouble.java
@@ -0,0 +1,16 @@
+package Warmup;
+
+public class sumDouble {
+
+    public static int sumDouble(int a, int b) {
+        if(a==b) return (a+b)*2;
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(sumDouble(2,3));
+
+    }
+
+}
