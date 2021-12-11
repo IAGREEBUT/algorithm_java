8c2ee39127b84c86a16a1fd73e9069b4d7d1fbb0
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-11 23:17:37 +0900

    String 1 : helloName

---
+++ CodingBat/src/String/helloName.java
@@ -0,0 +1,18 @@
+package String;
+
+public class helloName {
+
+    public static String helloName(String name) {
+
+        return "Hello " + name + "!";
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(helloName("YuJeong"));
+
+    }
+}
