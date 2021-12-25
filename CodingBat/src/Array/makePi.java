a92e1083c6124fd0008cf15a3813e6eea16f7297
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-25 23:54:22 +0900

    Array 1 : makePi

---
+++ CodingBat/src/Array/makePi.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class makePi {
+
+    public static int[] makePi() {
+
+        int []nums = {3,1,4};
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makePi());
+    }
+
+}
