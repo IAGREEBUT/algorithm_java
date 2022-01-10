a89f3180bb77a6f1543af08a5c540ceb7ed2a27e
YujeongLEE <dldbwjd24@ewhain.net>
2022-01-10 21:37:56 +0900

    Warmup 1 : or 35

--- CodingBat/src/Warmup/or35.java
+++ CodingBat/src/Warmup/or35.java
@@ -3,11 +3,12 @@ package Warmup;
 public class or35 {
 
     public static boolean or35(int n) {
-        
+        return (n%3==0 || n%5==0);
     }
 
 
     public static void main(String[] args) {
 
+        System.out.println(or35(10));
     }
 }
