8e9452d7b018441b51a9d10b2c9f172fb173e4ee
YujeongLEE <dldbwjd24@ewhain.net>
2022-01-26 23:48:04 +0900

    Warmup 1 : delDel

---
+++ CodingBat/src/Warmup/delDel.java
@@ -0,0 +1,17 @@
+package Warmup;
+
+public class delDel {
+
+    public static String delDel(String str) {
+
+        if(str.length()<4) return str;
+
+        if(str.substring(1,4).equals("del")) return str.substring(0,1)+str.substring(4);
+        return str;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(delDel("hdelh"));
+    }
+}
