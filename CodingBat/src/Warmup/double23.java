94c52d1013e252e18f349bf5bbe63eedc1a86c5d
YujeongLEE <dldbwjd24@ewhain.net>
2022-01-28 23:04:16 -0800

    Warmup 1 : double23

---
+++ CodingBat/src/Warmup/double23.java
@@ -0,0 +1,17 @@
+package Warmup;
+
+public class double23 {
+
+    public static boolean double23(int[] nums) {
+        if(nums.length < 2 ) return false;
+
+        return (nums[0]==2 && nums[1]==2) || (nums[0]==3 && nums[1]==3);
+    }
+
+
+    public static void main(String[] args) {
+        int []arr = {1,2};
+
+        System.out.println(double23(arr));
+    }
+}
