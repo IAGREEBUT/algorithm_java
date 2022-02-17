b4a95c6367433393011c694d6df510bbdf4749e2
YujeongLEE <dldbwjd24@ewhain.net>
2022-02-16 22:47:51 -0800

    Warmup 1 : in3050_solution

--- CodingBat/src/Warmup/in3050.java
+++ CodingBat/src/Warmup/in3050.java
@@ -6,6 +6,15 @@ public class in3050 {
         return ((a>=30 && a<=40) && (b>=30 && b<=40)) ||  ((a>=40 && a<=50) && (b>=40 && b<=50));
     }
 
+    public static boolean in3050_sol(int a, int b) {
+        if (a >= 30 && a <= 40 && b >= 30 && b <= 40) {
+            return true;
+        }
+        if (a >= 40 && a <= 50 && b >= 40 && b <= 50) {
+            return true;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         System.out.println(in3050(20,30));
