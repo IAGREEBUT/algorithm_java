32a835d4e9d3bc1f05b61e80b492a654753c9b69
YujeongLEE <dldbwjd24@ewhain.net>
2022-03-01 23:19:47 -0800

    Warmup 1 : everyNth

--- CodingBat/src/Warmup/everyNth.java
+++ CodingBat/src/Warmup/everyNth.java
@@ -13,6 +13,16 @@ public class everyNth {
         return ans;
     }
 
+
+    public static String everyNth_sol(String str, int n) {
+        String result = "";
+        
+        for (int i=0; i<str.length(); i = i + n) {
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println(everyNth("helloTHERE",3));
     }
